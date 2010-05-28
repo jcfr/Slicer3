NA-MIC/svn.slicer.org-Slicer4

commit 1169e40035804ac0f7babc52a19f8cd98f76def1

ENH: Speed improvement. Don't refresh when the scene is loading.
git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer4@13144 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Logic/vtkSlicerSliceLogic.cxx
@@ -259,7 +259,8 @@ void vtkSlicerSliceLogic::ProcessMRMLEvents(vtkObject * caller,
     && (event == vtkMRMLScene::NodeAddedEvent || event == vtkMRMLScene::NodeRemovedEvent ) )
     {
     vtkMRMLNode *node =  reinterpret_cast<vtkMRMLNode*> (callData);
-    if (node == NULL || !(node->IsA("vtkMRMLSliceCompositeNode") || node->IsA("vtkMRMLSliceNode") || node->IsA("vtkMRMLVolumeNode")) )
+    if (node == NULL || !(node->IsA("vtkMRMLSliceCompositeNode") || node->IsA("vtkMRMLSliceNode") || node->IsA("vtkMRMLVolumeNode")) ||
+        this->MRMLScene->GetIsClosed()) // don't do anything for performance reasons
       {
       return;
       }

Base/GUI/vtkSlicerSlicesControlGUI.cxx
@@ -74,7 +74,6 @@ vtkSlicerSlicesControlGUI::vtkSlicerSlicesControlGUI ( )
   this->YellowSliceEvents = NULL;
   this->GreenSliceEvents = NULL;
   this->EntryUpdatePending = 0;
-  this->SceneClosing = false;
   this->ProcessingMRMLEvent = 0;
   this->SliceInteracting = 0;
 
@@ -327,7 +326,7 @@ void vtkSlicerSlicesControlGUI::UpdateSliceGUIInteractorStyles ( )
 {
   // get all views from the scene
   // and observe active view.
-  if (this->SceneClosing)
+  if (this->MRMLScene && this->MRMLScene->GetIsClosed())
     {
     return;
     }
@@ -381,7 +380,7 @@ void vtkSlicerSlicesControlGUI::UpdateSliceGUIInteractorStyles ( )
 //---------------------------------------------------------------------------
 void vtkSlicerSlicesControlGUI::UpdateSlicesFromMRML()
 {
-  if (this->SceneClosing)
+  if (this->MRMLScene && this->MRMLScene->GetIsClosed())
     {
     return;
     }
@@ -1538,16 +1537,6 @@ void vtkSlicerSlicesControlGUI::ProcessMRMLEvents ( vtkObject *caller,
        && (event == vtkMRMLScene::NodeAddedEvent || event == vtkMRMLScene::NodeRemovedEvent ) )
     {
     this->UpdateFromMRML();
-    }
-
-  // is the scene closing?
-  if (event == vtkMRMLScene::SceneCloseEvent )
-    {
-    this->SceneClosing = true;
-    }
-  else 
-    {
-    this->SceneClosing = false;
     }
 
   // update FOV entry widgets to match node, if

Base/GUI/vtkSlicerSlicesControlGUI.h
@@ -190,13 +190,11 @@ class VTK_SLICER_BASE_GUI_EXPORT vtkSlicerSlicesControlGUI : public vtkSlicerCom
     int SliceInteracting;
     int ProcessingMRMLEvent;
     int EntryUpdatePending;
-    bool SceneClosing;
     float LastLabelOpacity;
 
  private:
     vtkSlicerSlicesControlGUI ( const vtkSlicerSlicesControlGUI& ); /// Not implemented.
     void operator = ( const vtkSlicerSlicesControlGUI& ); //Not implemented.
 };
-
 
 #endif

Base/GUI/vtkSlicerSlicesGUI.cxx
@@ -392,6 +392,11 @@ vtkSlicerSliceGUI* vtkSlicerSlicesGUI::GetNthSliceGUI(int n)
 
 void vtkSlicerSlicesGUI::UpdateGUI()
 {
+  if (this->GetMRMLScene() == NULL ||
+      this->GetMRMLScene()->GetIsClosed())
+    {
+    return;
+    }
   vtkSlicerNodeSelectorWidget *sliceNodeSelector = vtkSlicerNodeSelectorWidget::SafeDownCast( (*this->InternalParameterWidgetMap)["SliceNodeSelector"].GetPointer() );
 
   

Base/GUI/vtkSlicerViewControlGUI.cxx
@@ -60,7 +60,6 @@ vtkSlicerViewControlGUI::vtkSlicerViewControlGUI ( )
   this->NavigationRenderPending = 0;
   this->ZoomRenderPending = 0;
   this->EntryUpdatePending = 0;
-  this->SceneClosing = false;
   this->ProcessingMRMLEvent = 0;
   this->RockCount = 0;
   this->NavigationZoomWidgetWid = 150;
@@ -173,7 +172,6 @@ vtkSlicerViewControlGUI::~vtkSlicerViewControlGUI ( )
   this->NavigationRenderPending = 0;
   this->ZoomRenderPending = 0;
   this->EntryUpdatePending = 0;
-  this->SceneClosing = false;
   this->RockCount = 0;  
   this->SliceInteracting = 0;
 
@@ -616,7 +614,7 @@ void vtkSlicerViewControlGUI::UpdateSliceGUIInteractorStyles ( )
 {
   // get all views from the scene
   // and observe active view.
-  if (this->SceneClosing)
+  if (this->MRMLScene && this->MRMLScene->GetIsClosed())
     {
     return;
     }
@@ -679,7 +677,7 @@ void vtkSlicerViewControlGUI::UpdateFromMRML()
 void vtkSlicerViewControlGUI::UpdateSceneSnapshotsFromMRML()
 {
   
-  if (this->MRMLScene == NULL)
+  if (this->MRMLScene == NULL || this->MRMLScene->GetIsClosed())
     {
     return;
     }
@@ -783,7 +781,7 @@ void vtkSlicerViewControlGUI::DeleteSceneSnapshot(const char *nom )
 //---------------------------------------------------------------------------
 void vtkSlicerViewControlGUI::SetViewNode(vtkMRMLViewNode *node)
 {
-  if (this->SceneClosing || this->ViewNode == node)
+  if ((this->MRMLScene && this->MRMLScene->GetIsClosed()) || this->ViewNode == node)
     {
     return;
     }
@@ -815,7 +813,7 @@ void vtkSlicerViewControlGUI::UpdateViewFromMRML()
 //---------------------------------------------------------------------------
 void vtkSlicerViewControlGUI::UpdateSlicesFromMRML()
 {
-  if (this->SceneClosing)
+  if (this->MRMLScene && this->MRMLScene->GetIsClosed())
     {
     return;
     }
@@ -872,7 +870,10 @@ void vtkSlicerViewControlGUI::UpdateSlicesFromMRML()
 //---------------------------------------------------------------------------
 void vtkSlicerViewControlGUI::RequestNavigationRender()
 {
-
+  if (this->MRMLScene == NULL || this->MRMLScene->GetIsClosed())
+    {
+    return;
+    }
 
 #ifndef NAVZOOMWIDGET_DEBUG
   if(!this->EnableDisableNavButton->GetSelectedState())
@@ -3038,22 +3039,9 @@ void vtkSlicerViewControlGUI::ProcessMRMLEvents ( vtkObject *caller,
   this->ProcessingMRMLEvent = event;
 
   vtkDebugMacro("processing event " << event);
-   
-  if (event == vtkMRMLScene::SceneCloseEvent )
-    {
-    this->SceneClosing = true;
-    }
-  else 
-    {
-    this->SceneClosing = false;
-    }
-
 
   vtkMRMLViewNode *vnode = vtkMRMLViewNode::SafeDownCast ( caller );
   vtkMRMLSelectionNode *snode = vtkMRMLSelectionNode::SafeDownCast ( caller );
-  
-
-
 
   if ( this->GetMRMLScene() == NULL )
     {
@@ -3063,8 +3051,10 @@ void vtkSlicerViewControlGUI::ProcessMRMLEvents ( vtkObject *caller,
 
   // has a node been added or deleted?
   if ( vtkMRMLScene::SafeDownCast(caller) == this->MRMLScene 
-       && ((event == vtkMRMLScene::NodeAddedEvent || event == vtkMRMLScene::NodeRemovedEvent )
-           || (event == vtkMRMLScene::SceneCloseEvent) || (event == vtkMRMLScene::NewSceneEvent ) ) )
+       && (event == vtkMRMLScene::NodeAddedEvent || 
+           event == vtkMRMLScene::NodeRemovedEvent ||
+           event == vtkMRMLScene::SceneCloseEvent || 
+           event == vtkMRMLScene::SceneLoadEndEvent ) )
     {
     this->UpdateFromMRML();
     this->UpdateNavigationWidgetViewActors ( );
@@ -3988,7 +3978,7 @@ void vtkSlicerViewControlGUI::UpdateMainViewerInteractorStyles ( )
 {
   // get all views from the scene
   // and observe active view.
-  if (this->SceneClosing)
+  if (this->MRMLScene && this->MRMLScene->GetIsClosed())
     {
     return;
     }

jcfr/Slicer3

commit 38d14caf14e85418173fea53d0b2ce68f454927a

BUG: roll back optimization introduced in r13144 as it causes scenes that use the compare to not load poperly (the slice controller do not get wired to the slice nodes properly if the optization is used)
git-svn-id: http://svn.slicer.org/Slicer3/trunk@13552 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Logic/vtkSlicerSliceLogic.cxx
@@ -259,8 +259,7 @@ void vtkSlicerSliceLogic::ProcessMRMLEvents(vtkObject * caller,
     && (event == vtkMRMLScene::NodeAddedEvent || event == vtkMRMLScene::NodeRemovedEvent ) )
     {
     vtkMRMLNode *node =  reinterpret_cast<vtkMRMLNode*> (callData);
-    if (node == NULL || !(node->IsA("vtkMRMLSliceCompositeNode") || node->IsA("vtkMRMLSliceNode") || node->IsA("vtkMRMLVolumeNode")) ||
-        this->MRMLScene->GetIsClosed()) // don't do anything for performance reasons
+    if (node == NULL || !(node->IsA("vtkMRMLSliceCompositeNode") || node->IsA("vtkMRMLSliceNode") || node->IsA("vtkMRMLVolumeNode")) ) 
       {
       return;
       }