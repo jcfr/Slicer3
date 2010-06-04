39b16a3a95b2725136427ebc5884fe82e8c3f920
hayes <hayes@3bd1e089-480b-0410-8dfb-8563597acbee>
2010-06-04 15:02:05 +0000

    ENH: Changing version to RC3
    git-svn-id: http://svn.slicer.org/Slicer3/branches/Slicer-3-6@13629 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/vtkSlicerApplication.cxx
+++ Base/GUI/vtkSlicerApplication.cxx
@@ -270,7 +270,7 @@ vtkSlicerApplication::vtkSlicerApplication ( ) {
 
     // configure the application before creating
     // TODO: set name automatically from VERSION_PATCH or info in Version.txt
-    this->SetName ( "3D Slicer Version 3.6 RC2" );
+    this->SetName ( "3D Slicer Version 3.6 RC3" );
 
 #ifdef _WIN32
     vtkKWWin32RegistryHelper *regHelper =
