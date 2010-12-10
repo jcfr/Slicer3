f4d22938f7ed00de14cf3b764f80801d74d482e2
domibel <domibel@3bd1e089-480b-0410-8dfb-8563597acbee>
2010-12-10 19:25:55 +0000

    ENH: Threshold target image(s), negative values will be set to 0 so that the EMSegmenter can be used on those images.
    git-svn-id: http://svn.slicer.org/Slicer3/trunk@15639 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/EMSegment/vtkEMSegmentLogic.cxx
+++ Modules/EMSegment/vtkEMSegmentLogic.cxx
@@ -872,20 +872,6 @@ void vtkEMSegmentLogic::StartPreprocessingResampleAndCastToTarget(vtkMRMLVolumeN
       outputVolumeNode->GetImageData()->DeepCopy(cast->GetOutput());
       cast->Delete();
     }
-
-  double min = outputVolumeNode->GetImageData()->GetScalarRange()[0];
-  if (min < 0.0 )
-    {
-    vtkImageThreshold* thresh = vtkImageThreshold::New();
-    thresh->SetInput(outputVolumeNode->GetImageData());
-    thresh->ThresholdByLower(0.0);
-    thresh->SetInValue(0); // will it be ignored??
-    thresh->SetOutValue(0);
-    thresh->Update();
-    outputVolumeNode->GetImageData()->DeepCopy(thresh->GetOutput());
-    thresh->Delete();
-    }
-
   std::cout << "Resampling and casting output volume \"" << outputVolumeNode->GetName() << "\" to reference target \"" << fixedVolumeNode->GetName() <<  "\" DONE" << std::endl;
 }
 
