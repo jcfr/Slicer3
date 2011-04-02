681732ae9f596db2a1d6379b3b60e684d741bf21
domibel <domibel@3bd1e089-480b-0410-8dfb-8563597acbee>
2011-04-02 17:28:38 +0000

    ENH: Update test
    git-svn-id: http://svn.slicer.org/Slicer3/trunk@16295 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/EMSegment/Testing/test_atlas_creator.tcl
+++ Modules/EMSegment/Testing/test_atlas_creator.tcl
@@ -1,24 +1,36 @@
 #./Slicer3 --no-splash -p ../Slicer3/Modules/EMSegment/Testing/test_atlas_creator.tcl
-set mrmlScene $::slicer3::MRMLScene
 
 set Slicer3_HOME $::env(Slicer3_HOME)
 puts $Slicer3_HOME
 
-set targetFileName $Slicer3_HOME/../Slicer3/Modules/EMSegment/Tasks/MRI-Human-Brain/atlas_skulneck.nrrd
+
+set mrmlScene $::slicer3::MRMLScene
 $mrmlScene SetURL $Slicer3_HOME/../Slicer3/Modules/EMSegment/Tasks/MRI-Human-Brain.mrml
 
-set outputDir        "/tmp/AC/"
-set template         "$Slicer3_HOME/../Slicer3/Modules/AtlasCreator/TestData/originals/case62.nrrd"
-set segmentationsDir "$Slicer3_HOME/../Slicer3/Modules/AtlasCreator/TestData/segmentations/"
-set imagesDir        "$Slicer3_HOME/../Slicer3/Modules/AtlasCreator/TestData/originals/"
 
 puts "Start"
 # Load In volume 
+#set targetFileName "$Slicer3_HOME/../Slicer3/Modules/EMSegment/Tasks/MRI-Human-Brain/atlas_skulneck.nrrd"
+set targetFileName "$Slicer3_HOME/../Slicer3/Modules/AtlasCreator/TestData/originals/case62.nrrd"
+
 set MODVol [$::slicer3::Application GetModuleGUIByName "Volumes"]
 set MODLogic [$MODVol GetLogic]
-set targetNode [$MODLogic AddArchetypeVolume $targetFileName "TargetImage" 0 ]
+
+set MODEMS [$::slicer3::Application GetModuleGUIByName "EMSegmenter"]
+set mrmlManager [$MODEMS GetMRMLManager]
+
+set volumeNode [$MODLogic AddArchetypeVolume $targetFileName "TargetImage" 0]
+puts "[$volumeNode GetID]"
+$mrmlManager AddTargetSelectedVolumeByMRMLID [$volumeNode GetID]
+
+set targetNode [$workingDN GetAlignedTargetNode]
 
 source $Slicer3_HOME/../Slicer3/Modules/EMSegment/Tasks/GenericTask.tcl
-EMSegmenterPreProcessingTcl::AtlasCreator $template $segmentationsDir $imagesDir $outputDir "-1"
+
+set outputDir        "/tmp/AC/"
+set segmentationsDir "$Slicer3_HOME/../Slicer3/Modules/AtlasCreator/TestData/segmentations/"
+set imagesDir        "$Slicer3_HOME/../Slicer3/Modules/AtlasCreator/TestData/originals/"
+
+EMSegmenterPreProcessingTcl::AtlasCreator $segmentationsDir $imagesDir $outputDir $targetNode
 
 puts "End"
