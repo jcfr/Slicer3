aee07fd8b5745050077042afc0ad3deecad42ad7
haehn <haehn@3bd1e089-480b-0410-8dfb-8563597acbee>
2011-04-11 17:19:38 +0000

    ENH: AtlasCreator:
    - do not include the defaultCase in the actual atlas creation or the aligned mean image, if there is no segmentation for it available
    
    git-svn-id: http://svn.slicer.org/Slicer3/trunk@16376 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/AtlasCreator/AtlasCreatorLogic.py
+++ Modules/AtlasCreator/AtlasCreatorLogic.py
@@ -406,7 +406,11 @@ class AtlasCreatorLogic(object):
             # if we use fixed registration, we will now add the defaultCase as well
             # to prevent information loss during the combination stage
             if node.GetTemplateType() == "fixed":
-                alignedImages.append(defaultCase)            
+                # find the segmentation to the default case by looping through the segmentationsFilePathList
+                for s in segmentationsFilePathList:
+                    if os.path.basename(s) == os.path.basename(defaultCase):
+                        # this defaultCase has a segmentation so we do want to consider it in the atlas creation                
+                        alignedImages.append(defaultCase)
             
             self.ComputeMeanImage(schedulerCommand,
                                   alignedImages, 
